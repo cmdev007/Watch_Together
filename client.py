9ae4570b3d105ea1cb697354a482112234f4f6ae
Preet Patel <preet.makz@gmail.com>
2021-08-10 23:18:23 +0530

    CLIENT: Fine tuned syncing process removed jitters

--- client.py
+++ client.py
@@ -1,12 +1,22 @@
 import socket,time
 import os,json
 from _thread import start_new_thread
-PA_Inf_FLAG = False
+import argparse
 
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('-c', action='store_true')
+args = my_parser.parse_args()
+
+if args.c:
+    CFLAG = True
+
+PA_Inf_FLAG = False
+SEEK_FLAG = True
 host = '52.149.146.58'
 port = 1233
 
 def actioner():
+    global SEEK_FLAG
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
@@ -24,15 +34,17 @@ def actioner():
                 os.system('''echo '{ "command": ["set_property", "pause", false] }' | socat - /tmp/mpvsocket''')
             elif "PA" in data:
                 os.system('''echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/mpvsocket''')
-                SEEK = float(data.split("|")[-1])
-                os.system('''echo '{ "command": ["set_property", "time-pos", "''' + str(SEEK) + '''"], "request_id": 100 }' | socat - /tmp/mpvsocket''')
+                # SEEK = float(data.split("|")[-1])
+                # os.system('''echo '{ "command": ["set_property", "time-pos", "''' + str(SEEK) + '''"], "request_id": 100 }' | socat - /tmp/mpvsocket''')
             elif "SA" in data:
                 SEEK = float(data.split("|")[-1])
                 os.system('''echo '{ "command": ["set_property", "time-pos", "''' + str(SEEK) + '''"], "request_id": 100 }' | socat - /tmp/mpvsocket''')
+                SEEK_FLAG = False
 
 start_new_thread(actioner,())
 
 TIME = 0
+S_Counter = 0
 while(True):
     Old_Time = TIME
     #Realtime time sync
@@ -41,7 +53,7 @@ while(True):
         TIME = tdata['data']
     except:
         pass
-    if abs(TIME-Old_Time) > 2:
+    if abs(TIME-Old_Time) > 2 and SEEK_FLAG:
         ClientSocket = socket.socket()
 
         print('Waiting for connection')
@@ -102,7 +114,15 @@ while(True):
 
         ClientSocket.close()
         PA_Inf_FLAG = False
-    if not PSTATE:
-        time.sleep(0.5)
-    else:
+    
+    if not SEEK_FLAG:
+        if S_Counter < 4:
+            S_Counter+=1
+        else:
+            S_Counter = 0
+            SEEK_FLAG = True
+
+    if PSTATE and SEEK_FLAG:
         time.sleep(0.1)
+    else:
+        time.sleep(0.5)
